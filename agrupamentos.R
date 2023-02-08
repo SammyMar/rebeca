be2a33872489b0236e3a41c826a379dcb645c776
Samuel OObr <samuel.martins7@hotmail.com>
2023-02-08 18:01:42 -0300

    .

--- agrupamentos.R
+++ agrupamentos.R
@@ -54,25 +54,8 @@ clara1 <- cluster :: clara(df.agrup ,
 clara2 <- cluster :: clara(df.agrup ,
                            k = 5,
                            samples = 10)
-###############################################
-################### HIERARQUICOS ###############
-library(ggdendro)
-grup.dist <- dist(df.agrup)
-agl_ward <- hclust(grup.dist,method = "ward.D2")
 
- plot(cut(as.dendrogram (agl_ward), h = 20)$upper ,
-        main = "Ward - cortado em H = 20")
-
- agl_ward_res <- cutree(agl_ward , k = 3:5)
 ################################################
-# agl_single <-
-#    hclust(grup.dist , method = "single")
-#
-#  plot(cut(as.dendrogram (agl_single), h = 4)$upper ,
-#          main = "Vizinho mais P r x i m o - cortado em H = 4",
-#          xlab = "")
-#   agl_single_res <- cutree(agl_single , k = 3:8)
- #############################################
 agl_complete <-
  hclust(grup.dist , method = "complete")
 
@@ -104,5 +87,120 @@ agl_complete_res <- cutree(agl_complete , k = 2:5)
 ######## selecao melhor metodo #################
 df.agrup.completo |> head()
 df.agrup.completo |> colnames()
+df.agrup
+dados_diss <- dist(df.agrup,method = 'euclidian')
+# PAM 3
+pam_index_3 <- data.frame(
+metodo = "pam3",
+ db_index_cent = clusterSim::index.DB(df.agrup , pam1$clustering )$DB ,
+  db_index_med = clusterSim::index.DB(
+    df.agrup ,
+    pam1$clustering ,
+     d = dados_diss ,
+     centrotypes = "medoids"
+     )$DB ,
+   dunn_index = clValid::dunn(distance = dados_diss , pam1$clustering ),
+   silh_index = clusterSim::index.S(dados_diss , pam1$clustering ),
+   ch_index_cent = clusterSim::index.G1(df.agrup , pam1$clustering ),
+   ch_index_med = clusterSim::index.G1(df.agrup , pam1$clustering , d = dados_diss ,
+                                        centrotypes = "medoids")
+   )
+# PAM 4
+pam_index_4 <- data.frame(
+  metodo = "pam4",
+  db_index_cent = clusterSim::index.DB(df.agrup , pam1$clustering )$DB ,
+  db_index_med = clusterSim::index.DB(
+    df.agrup ,
+    pam1$clustering ,
+    d = dados_diss ,
+    centrotypes = "medoids"
+  )$DB ,
+  dunn_index = clValid::dunn(distance = dados_diss , pam1$clustering ),
+  silh_index = clusterSim::index.S(dados_diss , pam2$clustering ),
+  ch_index_cent = clusterSim::index.G1(df.agrup , pam2$clustering ),
+  ch_index_med = clusterSim::index.G1(df.agrup , pam2$clustering , d = dados_diss ,
+                                      centrotypes = "medoids")
+)
+# PAM 5
+pam_index_5 <- data.frame(
+  metodo = "pam5",
+  db_index_cent = clusterSim::index.DB(df.agrup , pam1$clustering )$DB ,
+  db_index_med = clusterSim::index.DB(
+    df.agrup ,
+    pam1$clustering ,
+    d = dados_diss ,
+    centrotypes = "medoids"
+  )$DB ,
+  dunn_index = clValid::dunn(distance = dados_diss , pam1$clustering ),
+  silh_index = clusterSim::index.S(dados_diss , pam3$clustering ),
+  ch_index_cent = clusterSim::index.G1(df.agrup , pam3$clustering ),
+  ch_index_med = clusterSim::index.G1(df.agrup , pam3$clustering , d = dados_diss ,
+                                      centrotypes = "medoids")
+)
+# CLARA 3
+clara_3_index <- data.frame(
+   metodo = "clara3",
+  db_index_cent = clusterSim::index.DB(df.agrup , clara1$clustering )$DB ,
+  db_index_med = clusterSim::index.DB(
+    df.agrup ,
+    clara1$clustering ,
+     d = dados_diss ,
+     centrotypes = "medoids"
+    )$DB ,
+  dunn_index = clValid::dunn(distance = dados_diss , clara1$clustering ),
+   silh_index = clusterSim::index.S(dados_diss , clara1$clustering ),
+  ch_index_cent = clusterSim::index.G1(df.agrup , clara1$clustering ),
+  ch_index_med = clusterSim::index.G1(df.agrup , clara1$clustering , d =
+                                             dados_diss , centrotypes = "medoids") )
 
-#davies bouldin ##########
+
+# CLARA 5
+clara_5_index <- data.frame(
+  metodo = "clara5",
+  db_index_cent = clusterSim::index.DB(df.agrup , clara2$clustering )$DB ,
+  db_index_med = clusterSim::index.DB(
+    df.agrup ,
+    clara2$clustering ,
+    d = dados_diss ,
+    centrotypes = "medoids"
+  )$DB ,
+  dunn_index = clValid::dunn(distance = dados_diss , clara2$clustering ),
+  silh_index = clusterSim::index.S(dados_diss , clara2$clustering ),
+  ch_index_cent = clusterSim::index.G1(df.agrup , clara2$clustering ),
+  ch_index_med = clusterSim::index.G1(df.agrup , clara2$clustering , d =
+                                        dados_diss , centrotypes = "medoids") )
+# KMEDIAS 5
+k_medias5_index <- data.frame(
+  metodo = "k_medias5",
+  db_index_cent = clusterSim::index.DB(df.agrup , k_medias1$cluster)$DB ,
+  db_index_med = clusterSim::index.DB(
+    df.agrup ,
+   k_medias1$cluster ,
+   d = dados_diss ,
+     centrotypes = "medoids"
+     )$DB ,
+   dunn_index = clValid::dunn(distance = dados_diss , k_medias1$cluster),
+   silh_index = clusterSim::index.S(dados_diss , k_medias1$cluster),
+   ch_index_cent = clusterSim::index.G1(df.agrup , k_medias1$cluster),
+   ch_index_med = clusterSim::index.G1(df.agrup , k_medias1$cluster , d =
+                                             dados_diss , centrotypes = "medoids"))
+
+# KMEDIAS 3
+k_medias3_index <- data.frame(
+  metodo = "k_medias3",
+  db_index_cent = clusterSim::index.DB(df.agrup , k_medias2$cluster)$DB ,
+  db_index_med = clusterSim::index.DB(
+    df.agrup ,
+    k_medias2$cluster ,
+    d = dados_diss ,
+    centrotypes = "medoids"
+  )$DB ,
+  dunn_index = clValid::dunn(distance = dados_diss , k_medias2$cluster),
+  silh_index = clusterSim::index.S(dados_diss , k_medias2$cluster),
+  ch_index_cent = clusterSim::index.G1(df.agrup , k_medias2$cluster),
+  ch_index_med = clusterSim::index.G1(df.agrup , k_medias2$cluster , d =
+                                        dados_diss , centrotypes = "medoids"))
+
+tabela1 <- rbind(pam_index_3,pam_index_4,
+                 pam_index_5,clara_3_index,clara_5_index,
+                 k_medias3_index,k_medias5_index)
